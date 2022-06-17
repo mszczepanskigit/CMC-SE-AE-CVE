c1a079a25045b6c3adc435f155334287bb178766
mateusz.szczepanski <83230824+mszczepanskigit@users.noreply.github.com>
2022-06-17 21:31:51 +0200

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,8 @@
+from math import log, exp
+from scipy.stats import norm
+import numpy as np
+from bs_exact import bs_exact
+from cmc_european import cmc_european
+from ant_european import ant_european
+from cmc_asian import cmc_asian
+from cv_european import cv_european
---
+++ main_file.py
@@ -0,0 +1,97 @@
+from imports import *
+
+if __name__ == "__main__":
+    # Exact Value
+    bs = bs_exact(s0=100, k=100, r=0.05, s=0.25)
+
+    print(u'\u2500' * 10)
+
+    """
+    # CMC European
+    cmc_eu_102 = cmc_european(s0=100, k=100, r=0.05, s=0.25, R=100)
+    cmc_eu_104 = cmc_european(s0=100, k=100, r=0.05, s=0.25, R=10000)
+    cmc_eu_106 = cmc_european(s0=100, k=100, r=0.05, s=0.25, R=1000000)
+
+    cmc_eu_102_err = abs((cmc_eu_102 - bs) / bs)
+    cmc_eu_104_err = abs((cmc_eu_104 - bs) / bs)
+    cmc_eu_106_err = abs((cmc_eu_106 - bs) / bs)
+
+    print("Exact value:", bs)
+    print("CMC European value R=10^2:", cmc_eu_102)
+    print("CMC European error R=10^2:", cmc_eu_102_err)
+    print("CMC European value R=10^4:", cmc_eu_104)
+    print("CMC European error R=10^4:", cmc_eu_104_err)
+    print("CMC European value R=10^6:", cmc_eu_106)
+    print("CMC European error R=10^6:", cmc_eu_106_err)
+
+    print(u'\u2500' * 10)
+    
+    # CMC Asian
+    cmc_as_102_2 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=100, n=2)
+    cmc_as_104_2 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=10000, n=2)
+    cmc_as_106_2 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=1000000, n=2)
+
+    cmc_as_102_3 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=100, n=3)
+    cmc_as_104_3 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=10000, n=3)
+    cmc_as_106_3 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=1000000, n=3)
+
+    cmc_as_102_17 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=100, n=17)
+    cmc_as_104_17 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=10000, n=17)
+    cmc_as_106_17 = cmc_asian(s0=100, k=100, r=0.05, s=0.25, R=1000000, n=17)
+
+    print("CMC Asian value R=10^2 and n=2 :", cmc_as_102_2)
+    print("CMC Asian value R=10^4 and n=2 :", cmc_as_104_2)
+    print("CMC Asian value R=10^6 and n=2 :", cmc_as_106_2)
+
+    print("CMC Asian value R=10^2 and n=3 :", cmc_as_102_3)
+    print("CMC Asian value R=10^4 and n=3 :", cmc_as_104_3)
+    print("CMC Asian value R=10^6 and n=3 :", cmc_as_106_3)
+
+    print("CMC Asian value R=10^2 and n=17:", cmc_as_102_17)
+    print("CMC Asian value R=10^4 and n=17:", cmc_as_104_17)
+    print("CMC Asian value R=10^6 and n=17:", cmc_as_106_17)
+
+    print(u'\u2500' * 10)
+    
+    # Stratified estimator
+    ---->TUTAJ WERA WKLEP SWOJE RZECZY<-----
+    
+    print(u'\u2500' * 10)
+
+    # Antithetic European
+    ant_eu_102 = ant_european(s0=100, k=100, r=0.05, s=0.25, R=100)
+    ant_eu_104 = ant_european(s0=100, k=100, r=0.05, s=0.25, R=10000)
+    ant_eu_106 = ant_european(s0=100, k=100, r=0.05, s=0.25, R=1000000)
+
+    ant_eu_102_err = abs((ant_eu_102 - bs) / bs)
+    ant_eu_104_err = abs((ant_eu_104 - bs) / bs)
+    ant_eu_106_err = abs((ant_eu_106 - bs) / bs)
+
+    print("Exact value:", bs)
+    print("Antithetic European value R=10^2:", ant_eu_102)
+    print("Antithetic European error R=10^2:", ant_eu_102_err)
+    print("Antithetic European value R=10^4:", ant_eu_104)
+    print("Antithetic European error R=10^4:", ant_eu_104_err)
+    print("Antithetic European value R=10^6:", ant_eu_106)
+    print("Antithetic European error R=10^6:", ant_eu_106_err)"""
+
+    print(u'\u2500' * 10)
+
+    # Control Variate European
+    cv_eu_102 = cv_european(s0=100, k=100, r=0.05, s=0.25, R=100)
+    cv_eu_104 = cv_european(s0=100, k=100, r=0.05, s=0.25, R=10000)
+    cv_eu_106 = cv_european(s0=100, k=100, r=0.05, s=0.25, R=1000000)
+
+    cv_eu_102_err = abs((cv_eu_102 - bs) / bs)
+    cv_eu_104_err = abs((cv_eu_104 - bs) / bs)
+    cv_eu_106_err = abs((cv_eu_106 - bs) / bs)
+
+    print("Exact value:", bs)
+    print("Control Variate European value R=10^2:", cv_eu_102)
+    print("Control Variate European error R=10^2:", cv_eu_102_err)
+    print("Control Variate European value R=10^4:", cv_eu_104)
+    print("Control Variate European error R=10^4:", cv_eu_104_err)
+    print("Control Variate European value R=10^6:", cv_eu_106)
+    print("Control Variate European error R=10^6:", cv_eu_106_err)
+
+    print(u'\u2500' * 10)
