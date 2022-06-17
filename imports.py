180a107a6c56eb3e760586b6d8b7b9d386ee99c4
WeronikaSzota <104278364+WeronikaSzota@users.noreply.github.com>
2022-06-17 22:17:39 +0200

    Update imports.py

--- imports.py
+++ imports.py
@@ -6,3 +6,7 @@ from cmc_european import cmc_european
 from ant_european import ant_european
 from cmc_asian import cmc_asian
 from cv_european import cv_european
+from stratified_1 import strat_prop
+from stratified_2 import strat_opt
+from stratified_1_2 import strat_prop2
+from stratified_2_2 import strat_opt2
