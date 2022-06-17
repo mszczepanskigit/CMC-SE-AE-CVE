bc70505e2729ccc7a4af5311e9f6b6d6052fa430
WeronikaSzota <104278364+WeronikaSzota@users.noreply.github.com>
2022-06-17 15:25:23 +0200

    Stratified estimator - optimal n=1

--- stratified_2.py
+++ stratified_2.py
@@ -1,25 +1,30 @@
 import numpy as np
-import random as rand
-from math import log, exp, sqrt
+from math import exp, sqrt
 from scipy.stats import norm
-from stratified_1 import strat_prop
 
 
-def strat_opt(s0, k, r, s, R, m):
+def strat_opt(s0, k, r, s, R, m, R_prim):
+    """
+       :param s0: S(0) - fixed
+       :param k: K - fixed
+       :param r: r - fixed
+       :param s: sigma - fixed
+       :param R: total number of replications
+       :param m: number of strata
+       :param R_prim: number of pilot replications
+       :return: estimated value
+    """
     # pilot simulations for estimating variance in each strata
-    R_prim = 1000
     est_s = []
     for i in range(m):
         V = []
         u = np.random.uniform(0, 1, int(R_prim / m))
         V = np.concatenate((V, (i / m + 1 / m * u)))
         z = norm.ppf(V)
-        mu = r - s ** 2 / 2
-        y_strat = np.mean(np.maximum(s0 * np.exp(mu + s * z) - k, 0))
         # estimated variance in one strata
-        est_s.append(sqrt(1 / (int(R_prim / m) - 1) * sum(np.power(z - y_strat, 2))))
-        # albo powinno byc: est_s.append(sqrt(1 / (int(R_prim / m) - 1) * sum(np.power(z - mean(z), 2))))
-    R_j = np.ceil((1 / m) * np.array(est_s) / sum((1 / m) * np.array(est_s) ) * R)
+        est_s.append(sqrt(np.var(z)))
+    # number of actual replications for each strata
+    R_j = np.round((1 / m) * np.array(est_s) / sum((1 / m) * np.array(est_s)) * R, 0)
     Y_est = []
     for i in range(m):
         V = []
@@ -27,8 +32,18 @@ def strat_opt(s0, k, r, s, R, m):
         V = np.concatenate((V, (i / m + 1 / m * u)))
         z = norm.ppf(V)
         mu = r - s ** 2 / 2
-        # estymator z kazdej warstwy
+        # estimator for each strata
         Y_est.append(np.mean(np.maximum(s0 * np.exp(mu + s * z) - k, 0)))
-    return np.mean(Y_est)
+    return exp(-r) * np.mean(Y_est)
 
-print(strat_opt(100, 100, 0.05, 0.25, 10000, 10))
+# np.random.seed(465726236011 % (2 ** 32 - 1))
+# print(strat_opt(100, 100, 0.05, 0.25, 10000, 5, 100))
+# print(strat_opt(100, 100, 0.05, 0.25, 10000, 10, 100))
+# print(strat_opt(100, 100, 0.05, 0.25, 10000, 5, 1000))
+# print(strat_opt(100, 100, 0.05, 0.25, 10000, 10, 1000))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 5, 100))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 10, 100))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 50, 100))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 5, 1000))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 10, 1000))
+# print(strat_opt(100, 100, 0.05, 0.25, 1000000, 50, 1000))
