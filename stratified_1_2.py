ad159f0d00da675729f19857e0f31202244c6638
WeronikaSzota <104278364+WeronikaSzota@users.noreply.github.com>
2022-06-17 15:24:41 +0200

    Stratified estimator - proportional n>1

--- stratified_1_2.py
+++ stratified_1_2.py
@@ -1,47 +1,65 @@
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
 import numpy as np
-from math import log, exp
+from math import exp
 from scipy.stats.distributions import chi2
 
 
 def strat_prop2(s0, k, r, s, R, m, n):
+    """
+    :param s0: S(0) - fixed
+    :param k: K - fixed
+    :param r: r - fixed
+    :param s: sigma - fixed
+    :param R: total number of replications
+    :param m: number of strata
+    :param n: dimension
+    :return: estimated value
+    """
     S = []
-    for i in range(1, m):
+    for i in range(1, n + 1):
         s_temp = []
-        for j in range(1, m):
+        for j in range(1, n + 1):
             s_temp.append(1 / n * min(i, j))
         S.append(s_temp)
     A = np.linalg.cholesky(S)
-    Z = np.zeros((n, R))
     Y = []
     for i in range(m):
+        Z = np.zeros((n, int(R / m)))
         xyz = []
-        # punkty
         norma = 0
         for j in range(n):
-            xyz.append(np.random.normal(0, 1, R))
+            xyz.append(np.random.normal(0, 1, int(R / m)))
             norma += xyz[j] ** 2
         norma = np.sqrt(norma)
         for j in range(n):
             xyz[j] = xyz[j] / norma
-        # promien
-        v = np.random.uniform(0, 1, R)
+        v = np.random.uniform(0, 1, int(R / m))
         v = v / m + i / m
         D2 = np.sqrt(chi2.ppf(v, df=n))
         for j in range(n):
             xyz[j] = xyz[j] * D2
             Z[j, :] = xyz[j]
         B = np.dot(A, Z)
-        # wyliczamy estymatory
         est_s = []
-        est_temp = np.zeros((1, R))
+        est_temp = np.zeros((1, int(R / m)))
         for g in range(n):
             est_s.append([s0 * np.exp((r - s ** 2 / 2) * (g + 1) / n + s * B[g, :])])
             est_temp += s0 * np.exp((r - s ** 2 / 2) * (g + 1) / n + s * B[g, :])
-        # estymacja w danej warstwie
+        # estimator for each strata
         Y.append(np.mean(np.maximum(1 / n * est_temp - k, 0)))
 
     print(exp(-r) * np.mean(Y))
 
-strat_prop2(100, 100, 0.05, 0.25, 1000, 4, 3)
+# np.random.seed(465726236011 % (2 ** 32 - 1))
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 4, 2)
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 4, 3)
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 4, 17)
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 10, 2)
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 10, 3)
+# strat_prop2(100, 100, 0.05, 0.25, 10000, 10, 17)
+#
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 4, 2)
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 4, 3)
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 4, 17)
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 10, 2)
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 10, 3)
+# strat_prop2(100, 100, 0.05, 0.25, 1000000, 10, 17)
